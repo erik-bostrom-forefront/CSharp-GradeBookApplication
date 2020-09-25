413523bc8c5da15ccc1e55b6199274e802f67848
erik-bostrom-forefront <erik.bostrom@forefront.se>
2020-09-25 16:06:25 +0200

    add ranked grading

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -8,18 +10,28 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name)
         {
-            Type = Enums.GradeBookType.Ranked;
+            Type = GradeBookType.Ranked;
         }
 
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException();
-                
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
             }
-            //return base.GetLetterGrade(averageGrade);
-            return 'F';
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToArray();
+
+            if (grades[threshold-1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
